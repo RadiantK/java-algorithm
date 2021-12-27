f5ab5f10c04c748f2808bfa4ea54a60f7dee1f3e
MinKook Kang <95058915+RadiantK@users.noreply.github.com>
2021-12-27 20:47:55 +0900

    패스트캠퍼스 알고리즘 문제

---
+++ Fastalgorithm/algorithm1/Algoritim1.java
@@ -0,0 +1,30 @@
+package algorithm1;
+
+// 배열중 가장 큰값, 가장 작은 값 찾기
+public class Algoritim1 {
+
+	public static void main(String[] args) {
+
+		int[] arr = {10, 55, 23, 2, 79, 101, 16, 82, 30, 45};
+		int max = arr[0];
+		int min = arr[0];
+		int minpos=0;
+		int maxpos=0;
+		
+		for(int i=1; i<arr.length; i++) {
+			if(max < arr[i]) {
+				max = arr[i];
+				maxpos = i+1;		
+			}
+			
+			if (min > arr[i]) {
+				min = arr[i];
+				minpos = i+1;
+			}
+		}
+		
+		System.out.println("큰값 : "+max+", 순서: "+maxpos);
+		System.out.println("작은값 : "+min+", 순서: "+minpos);
+	}
+
+}
---
+++ Fastalgorithm/algorithm2/Algorithm2.java
@@ -0,0 +1,44 @@
+package algorithm2;
+
+// 정렬된 수에서 하나의 수의 위치 찾기
+// 2진 탐색 활용 Binary Search
+public class Algorithm2 {
+
+	public static void main(String[] args) {
+
+		int[] arr = {12, 25, 31, 48, 54, 66, 70, 83, 95, 108};
+		
+		int target = 83;
+		int left = 0;
+		int right = arr.length-1;
+		int mid = (left+right) / 2;
+		
+		int temp = arr[mid];
+		boolean find = false;
+		
+		while(left<=right) {
+			
+			if(target == temp) {
+				find = true;
+				break;
+			}
+			else if (target < temp) {
+				right = mid - 1;
+			}
+			else {
+				left = mid + 1;
+			}
+			
+			mid = (left + right) / 2;
+			temp = arr[mid];
+		}
+		if(find == true)
+		{
+			mid++;
+			System.out.println("찾는 수는 "+ mid + "번째에 있습니다.");
+		} else {
+		System.out.println("찾는 수가 없습니다.");
+		}
+	}
+
+}
