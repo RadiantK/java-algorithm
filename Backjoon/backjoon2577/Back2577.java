9b24f1d5cfe3e280ed242f6999eea8788d97e5e2
RadiantK <pps8853@gmail.com>
2022-01-20 13:43:39 +0900

    백준 2577번 숫자의 개수

---
+++ Backjoon/backjoon2577/Back2577.java
@@ -0,0 +1,42 @@
+package backjoon2577;
+
+import java.util.Scanner;
+
+public class Back2577 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		sc.close();
+		
+		
+		int[] count = new int[10];
+		String str = String.valueOf(a*b*c);
+		
+		for(int i=0;i<str.length(); ++i) {
+			count[str.charAt(i) - '0']++;
+		}
+			
+		for(int i=0; i<count.length; ++i) {
+			System.out.println(count[i]);
+		}
+		
+//		int mul = a * b * c;
+//		int[] count = new int[10];
+//		System.out.println(mul);
+//		while(mul > 0) {
+//			count[mul % 10]++;
+//			mul /= 10;
+//		}
+//			
+//		for(int i=0; i<count.length; i++) {
+//			System.out.println(count[i]);
+//		}
+	}
+
+}
