2ae7797421df50826786cba809d1de0d394b6e78
RadiantK <pps8853@gmail.com>
2022-01-20 19:52:02 +0900

    백준 15596번 함수 : 정수 N개의 합

---
+++ Backjoon/backjoon15596/Back15596.java
@@ -0,0 +1,21 @@
+package backjoon15596;
+
+class Test {
+	
+	long sum(int[] a) {
+
+		long sum = 0;
+		for(int i=0; i<a.length; i++) {
+			sum += a[i];	
+		}
+		return sum;
+	}
+}
+
+public class Back15596 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
