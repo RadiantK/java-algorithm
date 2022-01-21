e8d2b67a70234b1c611350063f7fccc69a83b3db
RadiantK <pps8853@gmail.com>
2022-01-21 18:04:50 +0900

    백준 2941번 문자열 : 크로아티아 알파벳

--- Backjoon/backjoon2941/Back2941.java
+++ Backjoon/backjoon2941/Back2941.java
@@ -13,8 +13,8 @@ public class Back2941 {
 		String[] str = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
 		
 		for(int i=0; i<str.length; i++) {
-			if(s.contains(str[i])) {
-				s = s.replaceAll(str[i], " ");
+			if(s.contains(str[i])) { // str[i]번째 문자가 포함되어있으면 조건문 실행
+				s = s.replaceAll(str[i], " "); // [str[i]문자를 공백으로 변경
 			}
 		}
 		System.out.println(s.length());
