abc4239f93c28f8ba3568328123a81022d20b854
RadiantK <pps8853@gmail.com>
2022-01-22 16:20:58 +0900

    백준 11654번 정수 아스키코드로 변환

---
+++ Backjoon/backjoon11654/Back11654.java
@@ -0,0 +1,17 @@
+package backjoon11654;
+
+import java.util.Scanner;
+
+public class Back11654 {
+
+	// 아스키코드 숫자로 변환
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.next().charAt(0);
+		System.out.println(a);
+		sc.close();
+	}
+
+}
