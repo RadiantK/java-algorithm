f97de016288f101ee68fc8fe11b54bb99df819a1
RadiantK <pps8853@gmail.com>
2022-01-24 23:39:28 +0900

    백준 11729번 재귀함수 : 하노이의탑

---
+++ Backjoon/backjoon11729/Back11729.java
@@ -0,0 +1,35 @@
+package backjoon11729;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Back11729 {
+
+	static int count=0;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		br.close();
+		hanoi(n, 1, 2, 3);
+		System.out.println(count);
+		System.out.println(sb);
+		
+	}
+	
+	static void hanoi(int n, int from, int by, int to) {
+		count++;
+		if(n==1) { // 원판이 1개일 때
+			sb.append(from+" "+to+"\n");
+			return;
+		}
+		// 1 -> 3 -> 2 / 2 -> 1 -> 3 이동순서
+		hanoi(n-1, from, to, by); // n-1개를 1 -> 2로 이동
+		sb.append(from + " " + to + "\n"); // 1개를 1 -> 3으로 이동
+		hanoi(n-1, by, from, to); // n-1개를 2 -> 3으로 이동		
+	}		
+}
