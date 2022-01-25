4139bdd2d0c313533cd5aae1f80def8f56c9ab4a
RadiantK <pps8853@gmail.com>
2022-01-25 21:31:53 +0900

    알고리즘 기초4번 : 진수를 2진수로 변환

---
+++ BasicAlgorithm/algorithm4/Binary.java
@@ -0,0 +1,32 @@
+package algorithm4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Binary {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int input = sc.nextInt();
+		
+		System.out.println(Integer.toBinaryString(input));
+		
+		int[] binary = new int[10];
+		int mok = input;
+		int index = 0;
+		
+		while (mok > 0) {
+			binary[index] = mok % 2;
+			mok = mok / 2;
+			index++;
+		}
+		index--;
+
+		for (; index>=0; index--) {
+			System.out.print(binary[index]);
+		}
+	}
+
+}
