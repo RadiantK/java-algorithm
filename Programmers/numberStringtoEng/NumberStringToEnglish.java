bf42e4c9de688d43e7369f25fe8551b99d56ab17
RadiantK <pps8853@gmail.com>
2022-01-30 13:52:50 +0900

    프로그래머스 lv1 : 숫자문자열과 영단어

---
+++ Programmers/numberStringtoEng/NumberStringToEnglish.java
@@ -0,0 +1,41 @@
+package numberStringtoEng;
+
+class Solution {
+    public int solution(String s) {   
+    	
+    	String[] nums = {"0","1","2","3","4","5","6","7","8","9"};
+    	String[] str = 
+    		{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    	
+    	for(int i = 0; i < str.length; i++) {
+    		s = s.replace(str[i], nums[i]);
+    	}
+    	
+        return Integer.parseInt(s);
+    }
+}
+
+class Solution2 {
+    public int solution(String s) {   
+    	
+    	String[] str = 
+    		{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    	
+    	for(int i = 0; i < str.length; i++) {
+    		s = s.replace(str[i], String.valueOf(i));
+    	}
+    	
+        return Integer.parseInt(s);
+    }
+}
+
+public class NumberStringToEnglish {
+
+	public static void main(String[] args) {
+
+		Solution s = new Solution();
+		
+		System.out.println(s.solution("one4seveneight"));
+	}
+
+}
