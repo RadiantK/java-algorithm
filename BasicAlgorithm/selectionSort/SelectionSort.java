23d0c9eb277a8283fe30a39f1c2efcf24a6b5483
RadiantK <pps8853@gmail.com>
2022-02-04 19:24:20 +0900

    선택 정렬

--- BasicAlgorithm/selectionSort/SelectionSort.java
+++ BasicAlgorithm/selectionSort/SelectionSort.java
@@ -8,11 +8,13 @@ public class SelectionSort {
 		selection_sort(a, a.length);
 	}
 
+	// 선택 정렬 (최솟값 정렬)
 	private static void selection_sort(int[] a, int size) {
 		
 		for(int i = 0; i < size -1; i++) {
 			int min_idx = i;
 			
+			// 배열에서 min_idx 위치의 값보다 작은 값을 min_idx의 위치와 바꿔줌 
 			for(int j = i+1; j < size; j++) {
 				if(a[j] < a[min_idx]) {
 					min_idx = j;
