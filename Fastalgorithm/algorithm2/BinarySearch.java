daa322dd181123898a4339b370eba8a3a3c15994
RadiantK <pps8853@gmail.com>
2022-02-05 23:50:02 +0900

    이코테 2진탐색 : 정렬된 배열에서 특정 수의 개수 구하기

--- Fastalgorithm/algorithm2/BinarySearch.java
+++ Fastalgorithm/algorithm2/BinarySearch.java
@@ -2,13 +2,13 @@ package algorithm2;
 
 // 정렬된 수에서 하나의 수의 위치 찾기
 // 2진 탐색 활용 Binary Search
-public class Algorithm2 {
+public class BinarySearch {
 
 	public static void main(String[] args) {
 
 		int[] arr = {12, 25, 31, 48, 54, 66, 70, 83, 95, 108};
 
-		int target = 54;
+		int target = 70;
 		int left = 0;
 		int right = arr.length-1;
 		int mid = (left+right) / 2;
@@ -29,8 +29,7 @@ public class Algorithm2 {
 			temp = arr[mid];
 		}
 		
-		if(find == true)
-		{
+		if(find == true) {
 			mid++;
 			System.out.println("찾는 수는 "+ mid + "번째에 있습니다.");
 		} else {
